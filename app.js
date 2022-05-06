5fb814db0a5b1c5fb128fbf5bf2861e0af5505b4
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-05-06 14:09:00 -0300

    Fix: unable do display pokemon image from PokeAPI

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ const fetchPokemon = async () => {
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
-
+console.log(pokemons)
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-
                 accumulator +=
                     `<li class="card ${types[0]}">
-                 <img class"card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
-            <h2 class="card-title">${pokemon.name}. ${pokemon.id} </h2>
+                 <img class"card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
             <p class="card-subtitle">${types.join(' | ')}</p>
              </li>`
                 return accumulator
@@ -30,6 +29,8 @@ const fetchPokemon = async () => {
 const ul = document.querySelector('[data-js="pokedex"]')
 
             ul.innerHTML = lisPokemons
+            
         })
 }
 fetchPokemon();
+
